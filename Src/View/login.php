c7cc0d1ff2a493786b6e3c36bfd4c97888c9c2dc
TiagoUniverse <tiagocesar68@gmail.com>
2023-12-15 23:37:20 -0300

    Adicionado o cadastro de novas contas na tela de login!

--- Src/View/login.php
+++ Src/View/login.php
@@ -63,8 +63,6 @@ if (isset($_POST['status_cadastro']) && $_POST['status_cadastro'] == "FINALIZAND
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="../../Assets/Img/icons/favicon.ico" />
-    <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="../../Assets/vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="../../Assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
